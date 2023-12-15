ca701db07e7876c3da35456a824b36d4b0ac5e00
DebugDynasty <zilli7733@gmail.com>
2023-12-15 16:05:53 +0200

    Add project files.

---
+++ ConsoleApp4/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Status { get; set; }
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Status = "Available";
+        }
+        public void Reserve()
+        {
+            if (Status == "Available")
+            {
+                Status = "Reserved";
+                Console.WriteLine($"{Title} has been reserved.");
+            }
+            else
+            {
+                Console.WriteLine($"{Title} is not available for reservation.");
+            }
+        }
+
+        public void CancelReservation()
+        {
+            if (Status == "Reserved")
+            {
+                Status = "Available";
+                Console.WriteLine($"Reservation for {Title} has been canceled.");
+            }
+            else
+            {
+                Console.WriteLine($"No reservation found for {Title}.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Reservation
+    {
+        public static void ReserveBook(Book book)
+        {
+            book.Reserve();
+        }
+
+        public static void CancelReservation(Book book)
+        {
+            book.CancelReservation();
+        }
+    }
+}
